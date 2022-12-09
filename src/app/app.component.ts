0627b93cee533bdaab52dc213492ec58fa139e36
Jasmin Dedic <jdedic1999@gmail.com>
2022-12-09 20:29:09 +0100

    started header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['coding_2.jpg', 'coding.jpg'];
-  currentImage = 0;
-  showImage = true;
+export class AppComponent {
 
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
 
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+  images = ['coding_2.jpg', 'coding.jpg', 'coding_3.jpg'];
+  headlines = ['Bring engineering to the next level',
+    'Born to code',
+    'Coding is passion'
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
+}
