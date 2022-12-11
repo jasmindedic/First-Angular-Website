04e107ba71d0394c1e7257ebfa38d658a4bf9660
Jasmin Dedic <jdedic1999@gmail.com>
2022-12-11 15:43:54 +0100

    Added more features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(public router: Router) { }
 
 }
